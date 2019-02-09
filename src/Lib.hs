c351a2286bf5659371e6b63be3ee3ec0e35135f9
Francososa <franky.sosa@gmail.com>
2019-02-09 12:52:20 -0600

    Refactor Lib to use new types

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,30 +3,47 @@ module Lib where
 import Data.Char
 
 
-checkPasswordLength :: String -> Either String String
+newtype Password = Password String
+  deriving Show
+
+newtype Error = Error String
+  deriving Show
+
+newtype Username = Username String
+  deriving Show
+
+
+checkPasswordLength :: String -> Either Error Password
 checkPasswordLength password =
   case (length password > 20 || length password < 10) of
-    True -> Left "Your password has to be between 10 and 20 characters long"
-    False -> Right password
+    True -> Left (Error "Your password has to be between 10 and 20 characters long")
+    False -> Right (Password password)
 
 
-requireAlphaNum :: String -> Either String String
+checkUsernameLength :: String -> Either Error Username
+checkUsernameLength name =
+  case (length name > 15) of
+    True -> Left (Error "Username cannot be longer than 15 characters.")
+    False -> Right (Username name)
+
+
+requireAlphaNum :: String -> Either Error String
 requireAlphaNum xs =
   case (all isAlphaNum xs) of
     True -> Right xs
-    False -> Left "Your password cannot contain whitespace or special characters."
+    False -> Left (Error "Your password cannot contain whitespace or special characters.")
 
 
-cleanWhitespace :: String -> Either String String
-cleanWhitespace "" = Left "Your password cannot be empty"
+cleanWhitespace :: String -> Either Error String
+cleanWhitespace "" = Left (Error "Your password cannot be empty")
 cleanWhitespace (x : xs) =
   case (isSpace x) of
     True -> cleanWhitespace xs
     False -> Right (x : xs)
 
 
-validatePassword :: String -> Either String String
-validatePassword password =
+validatePassword :: Password -> Either Error Password
+validatePassword (Password password) =
   cleanWhitespace password
   >>= requireAlphaNum
   >>= checkPasswordLength
