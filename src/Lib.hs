4b3814592e3c8c1968a1b67d5af26f84a04d7364
Francososa <franky.sosa@gmail.com>
2019-02-08 19:24:54 -0600

    Combine password validation functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,7 +11,7 @@ someFunc = putStrLn "someFunc"
 
 checkPasswordLength :: String -> Maybe String
 checkPasswordLength password =
-  case (length password > 20) of
+  case (length password > 20 || length password < 10) of
     True -> Nothing
     False -> Just password
 
@@ -29,3 +29,16 @@ cleanWhitespace (x : xs) =
   case (isSpace x) of
     True -> cleanWhitespace xs
     False -> Just (x : xs)
+
+
+passwordValidation :: String -> Maybe String
+passwordValidation password =
+  case (cleanWhitespace password) of
+    Nothing -> Nothing
+    Just password2 ->
+      case (requireAlphaNum password2) of
+        Nothing -> Nothing
+        Just password3 ->
+          case (checkPasswordLength password3) of
+            Nothing -> Nothing
+            Just password4 -> Just password4
