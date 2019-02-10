f5d984a84b8ef84d477b0a321c810f18bc9471e3
Francososa <franky.sosa@gmail.com>
2019-02-10 14:43:56 -0600

    Add username validation to Main

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,11 @@ import Lib
 main :: IO ()
 main =
   do
-    putStrLn "Please enter a password"
+    putStrLn "Please enter a username."
+    username <- Username <$> getLine
+    putStrLn "Please enter a password."
     password <- Password <$> getLine
+    print (validateUsername username)
     print (validatePassword password)
 
 
