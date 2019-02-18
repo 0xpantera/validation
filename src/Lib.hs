c93c19c07e07006f6655d0f4c2af891f8d3ab89d
Francososa <franky.sosa@gmail.com>
2019-02-17 18:34:37 -0600

    Refactor code to use Validation type instead of Either

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Lib where
 
 import Data.Char
+import Data.Validation
+import Data.Semigroup
 
 
 newtype Password = Password String
   deriving Show
 
-newtype Error = Error String
-  deriving (Show, Eq)
+
+newtype Error = Error [String]
+  deriving (Show, Semigroup)
+
 
 newtype Username = Username String
   deriving Show
@@ -17,57 +22,57 @@ data User = User Username Password
   deriving Show
 
 
-checkPasswordLength :: String -> Either Error Password
+checkPasswordLength :: String -> Validation Error Password
 checkPasswordLength password =
   case (length password > 20 || length password < 10) of
-    True -> Left (Error "Your password has to be between 10 and 20 characters long")
-    False -> Right (Password password)
+    True -> Failure (Error ["Your password has to be between 10 and 20 characters long"])
+    False -> Success (Password password)
 
 
-checkUsernameLength :: String -> Either Error Username
+checkUsernameLength :: String -> Validation Error Username
 checkUsernameLength name =
   case (length name > 15) of
-    True -> Left (Error "Username cannot be longer than 15 characters.")
-    False -> Right (Username name)
+    True -> Failure (Error ["Username cannot be longer than 15 characters."])
+    False -> Success (Username name)
 
 
-checkLength :: Int -> String -> Either Error String
+checkLength :: Int -> String -> Validation Error String
 checkLength n field =
   case (length field > n) of
-    True -> Left (Error ("Fields cannot be longer than " ++ (show n) ++ " characters"))
-    False -> Right field
+    True -> Failure (Error ["Fields cannot be longer than " ++ (show n) ++ " characters"])
+    False -> Success field
 
 
-requireAlphaNum :: String -> Either Error String
+requireAlphaNum :: String -> Validation Error String
 requireAlphaNum xs =
   case (all isAlphaNum xs) of
-    True -> Right xs
-    False -> Left (Error "Your password cannot contain whitespace or special characters.")
+    True -> Success xs
+    False -> Failure (Error ["Your password cannot contain whitespace or special characters."])
 
 
-cleanWhitespace :: String -> Either Error String
-cleanWhitespace "" = Left (Error "Your password cannot be empty")
+cleanWhitespace :: String -> Validation Error String
+cleanWhitespace "" = Failure (Error ["Your password cannot be empty"])
 cleanWhitespace (x : xs) =
   case (isSpace x) of
     True -> cleanWhitespace xs
-    False -> Right (x : xs)
+    False -> Success (x : xs)
 
 
-validatePassword :: Password -> Either Error Password
+validatePassword :: Password -> Validation Error Password
 validatePassword (Password password) =
   cleanWhitespace password
   >>= requireAlphaNum
   >>= checkPasswordLength
 
 
-validateUsername :: Username -> Either Error Username
+validateUsername :: Username -> Validation Error Username
 validateUsername (Username username) =
   cleanWhitespace username
   >>= requireAlphaNum
   >>= checkUsernameLength
 
 
-doValidateUsername :: Username -> Either Error Username
+doValidateUsername :: Username -> Validation Error Username
 doValidateUsername (Username username) =
   do
     cleanUser <- cleanWhitespace username
@@ -75,14 +80,15 @@ doValidateUsername (Username username) =
     checkUsernameLength alphaNumUser
 
 
-makeUser :: Username -> Password -> Either Error User
+makeUser :: Username -> Password -> Validation Error User
 makeUser name pass =
   User <$> (validateUsername name)
        <*> (validatePassword pass)
 
 
-makeUserTmpPassword :: Username -> Either Error User
+makeUserTmpPassword :: Username -> Validation Error User
 makeUserTmpPassword name =
   User <$> validateUsername name
        <*> pure (Password "temporaryPassword")
+
 
